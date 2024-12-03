5b97c3012adfbcc2eae9916dd153cd01ceb42aa8
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-12-03 13:30:32 +0000

    I think I did it right this time

--- bank_account.py
+++ bank_account.py
@@ -1,13 +1,17 @@
 class Bank_account:
-    def __init__(self, owner) -> None:
-        self.owner = owner
-        self.balance = 0
+    def __init__(self, owner, balance = 0) -> None:
+        self.__owner = owner
+        self.__balance = balance
     def deposit(self, money):
-        self.balance = self.balance + money
+        self.__balance = self.__balance + money
     def withdraw(self, money):
-        if money <= self.balance:
-            self.balance = self.balance - money
+        if money <= self.__balance:
+            self.__balance = self.__balance - money
         else:
             print("Insufficient funds")
+    def get_owner(self):
+        return self.__owner
+    def set_owner(self, new_name):
+        self.__owner = new_name
     def get_balance(self):
-        return self.balance
+        return self.__balance
