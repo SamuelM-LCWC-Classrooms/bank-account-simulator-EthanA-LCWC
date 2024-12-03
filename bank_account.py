17483f4456f90175bedec44d6f0bfc7633241095
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-12-03 13:16:07 +0000

    I did this in visual studio and then copied it into here

--- bank_account.py
+++ bank_account.py
@@ -1,1 +1,13 @@
-
+class BankAccount:
+    def __init__(self, owner) -> None:
+        self.owner = owner
+        self.balance = 0
+    def deposit(self, money):
+        self.balance = self.balance + money
+    def withdraw(self, money):
+        if money <= self.balance:
+            self.balance = self.balance - money
+        else:
+            print("Insufficient funds")
+    def get_balance(self):
+        return self.balance
